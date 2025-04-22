86e267911994f7fd16f7594c9f1005738d036072
MariaJenson96 <mariajenson1996@gmail.com>
2025-04-23 00:34:50 +0530

    feature/ dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { SecondpageComponent } from './pages/secondpage/secondpage.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
-    {path:'home', component:AppComponent, children:[
-        {path:'', component: DashboardComponent},
-        {path:'second', component:SecondpageComponent}
-    ]}
+    {path:'home', component:DashboardComponent}
 ];
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    NgApexchartsModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
